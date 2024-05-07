13b0ccb811cbd84e2df5238ae725d62f49889d9f
Radwa Mohamed <Radwa.Mohamed@Dsquares.onmicrosoft.com>
2024-05-07 15:26:34 +0300

    Add project files.

---
+++ Data Integration/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data_Integration
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           // GlobalQueryFilters.CreateGlobalIsDeletedQueryFilter(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Data Integration/DbContext/GlobalQueryFilters.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DeliveryIntegration.DbContext
+{
+    public static class GlobalQueryFilters
+    {
+        public static void CreateGlobalIsDeletedQueryFilter(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Model
+                .GetEntityTypes()
+                .ToList()
+                .ForEach(et =>
+                {
+                    //// Check if the entity implements ISoftDelete
+                    //if (typeof(ISoftDelete).IsAssignableFrom(et.ClrType))
+                    //{
+                    //    // Creates a parameter to be used in lambda expression ex: (e => e) e is the parameter
+                    //    var parameter = Expression.Parameter(et.ClrType, "e");
+
+                    //    // Creates the body to be used in creating a lambda expression
+                    //    var body = Expression
+                    //    .Equal(
+                    //        Expression.Call(typeof(EF), nameof(EF.Property), new[] { typeof(bool) }, parameter, Expression.Constant(nameof(ISoftDelete.IsDeleted))),
+                    //            Expression.Constant(false));
+
+                    //    // Adds the global query filter
+                    //    modelBuilder.Entity(et.ClrType).HasQueryFilter(Expression.Lambda(body, parameter));
+                    //}
+                });
+        }
+    }
+}
